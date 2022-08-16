9a8518cd87e45fae4e92e9838e5a9bc682c3de48
Ihor Kharchenko <ihor.stavr@gmail.com>
2022-08-16 23:18:58 +0300

    formik

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,27 @@
+import { Formik, Form, Field } from 'formik';
+
+const initialValues = {
+  login: '',
+  password: '',
+};
+export const LoginForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="off">
+        <label htmlFor="login">
+          login
+          <Field type="text" name="login" />
+        </label>
+        <label htmlFor="password">
+          password
+          <Field type="password" name="password" />
+        </label>
+        <button type="submit">submit</button>
+      </Form>
+    </Formik>
+  );
+};
